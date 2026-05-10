18ab3b79587716a10ead7c6af14c8b76f9ab02af
Aryan Vasudeva <aryanvasudeva@hotmail.com>
2026-05-10 16:35:40 +0530

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Aryan Vasudeva on 10/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
